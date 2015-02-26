b2e5ce365a77d67b9a9b54e162f80e25852ea2f0
Roger Ye <shugaoye@yahoo.com>
2015-02-26 18:29:53 +0800

    Integrated U-Boot, kernel and AOSP.

--- full_armemu.mk
+++ full_armemu.mk
@@ -30,30 +30,34 @@ PRODUCT_DEVICE := armemu
 PRODUCT_BRAND := AOSP_ARMEMU
 PRODUCT_MODEL := Full_Android_ARMEMU
 
-# KERNEL_DEFCONFIG := goldfish_armv7_defconfig
-# KERNEL_DIR := kernel
+# define U-Boot and Kernel path and configuration
 TARGET_U_BOOT_SOURCE := u-boot
 TARGET_U_BOOT_CONFIG := goldfish_config
 
-# TARGET_PREBUILT_U_BOOT := device/generic/armemu/u-boot.bin
 TARGET_KERNEL_SOURCE := kernel
 TARGET_KERNEL_CONFIG := goldfish_armv7_defconfig
+
 PRODUCT_OUT ?= out/target/product/armemu
+
 include $(TARGET_KERNEL_SOURCE)/AndroidKernel.mk
-
-# device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
-# So cp will do.
-.PHONY: $(PRODUCT_OUT)/kernel $(PRODUCT_OUT)/u-boot.bin
-$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
-	echo "==> After kernel build..."
-	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
-
 include $(TARGET_U_BOOT_SOURCE)/AndroidU-Boot.mk
-$(PRODUCT_OUT)/u-boot.bin: $(TARGET_PREBUILT_U-BOOT)
-	echo "==> After U-Boot build..."
-	cp $(TARGET_PREBUILT_U-BOOT) $(PRODUCT_OUT)/u-boot.bin
-	mkimage -A arm -C none -O linux -T kernel -d $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/arm/boot/zImage -a 0x00010000 -e 0x00010000 $(PRODUCT_OUT)/system/zImage.uimg
-	mkimage -A arm -C none -O linux -T ramdisk -d $(PRODUCT_OUT)/ramdisk.img -a 0x00800000 -e 0x00800000 $(PRODUCT_OUT)/system/rootfs.uimg
+
+# define U-Boot images
+TARGET_KERNEL_UIMAGE := $(PRODUCT_OUT)/zImage.uimg
+TARGET_RAMDISK_UIMAGE := $(PRODUCT_OUT)/ramdisk.uimg
+TARGET_RECOVERY_UIMAGE := $(PRODUCT_OUT)/ramdisk-recovery.uimg
+
+# define build targets for kernel, U-Boot and U-Boot images
+.PHONY: $(TARGET_PREBUILT_KERNEL) $(TARGET_PREBUILT_U-BOOT) $(TARGET_KERNEL_UIMAGE) $(TARGET_RAMDISK_UIMAGE) $(TARGET_RECOVERY_UIMAGE)
+
+$(TARGET_KERNEL_UIMAGE): $(TARGET_PREBUILT_KERNEL)
+	mkimage -A arm -C none -O linux -T kernel -d $(TARGET_PREBUILT_INT_KERNEL) -a 0x00010000 -e 0x00010000 $(TARGET_KERNEL_UIMAGE)
+
+$(TARGET_RAMDISK_UIMAGE): $(PRODUCT_OUT)/ramdisk.img
+	mkimage -A arm -C none -O linux -T ramdisk -d $(PRODUCT_OUT)/ramdisk.img -a 0x00800000 -e 0x00800000 $(TARGET_RAMDISK_UIMAGE)
+
+$(TARGET_RECOVERY_UIMAGE): $(PRODUCT_OUT)/ramdisk-recovery.img
+	mkimage -A arm -C none -O linux -T ramdisk -d $(PRODUCT_OUT)/ramdisk-recovery.img -a 0x00800000 -e 0x00800000 $(TARGET_RECOVERY_UIMAGE)
 
 LOCAL_U_BOOT := $(TARGET_PREBUILT_U-BOOT)
 
@@ -63,14 +67,16 @@ else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+LOCAL_KERNEL_UIMAGE := $(TARGET_KERNEL_UIMAGE)
+LOCAL_RAMDISK_UIMAGE := $(TARGET_RAMDISK_UIMAGE)
+LOCAL_RECOVERY_UIMAGE := $(TARGET_RECOVERY_UIMAGE)
+
 PRODUCT_COPY_FILES += \
      $(LOCAL_U_BOOT):u-boot.bin \
      $(LOCAL_KERNEL):kernel \
+     $(LOCAL_KERNEL_UIMAGE):system/zImage.uimg \
+     $(LOCAL_RAMDISK_UIMAGE):system/ramdisk.uimg \
+     $(LOCAL_RECOVERY_UIMAGE):system/ramdisk-recovery.uimg \
      device/generic/armemu/init.recovery.armemu.rc:root/init.recovery.armemu.rc \
      device/generic/armemu/init.recovery.armemu.sh:root/init.recovery.armemu.sh
-
-#     $(LOCAL_KERNEL):system/kernel \
-#     out/target/product/armemu/ramdisk.img:system/ramdisk.img \
-
-# DEVICE_PACKAGE_OVERLAYS := device/generic/armemu/overlay
 
